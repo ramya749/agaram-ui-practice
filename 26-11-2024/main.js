22e5dbc0d81e262942ba1984925f71b531222430
ramya749 <ramyapharathi@gmail.com>
2024-11-26 21:49:17 -0400

    resume

---
+++ 26-11-2024/main.js
@@ -0,0 +1,89 @@
+
+let resume={
+    name   : "M.RAMYA BHARATHI",
+    emailid: "ramyapharathi@gmail.com",
+    mobile : 9786524255,
+    address: "1/22 pilaiyarcoilstreet,kothaigrammam",
+    
+    education:[
+    {
+        course    : "B.sc Maths",
+        year      : "(2017-2020)",
+        Institute : "Pioneer Kumaraswamy College",
+        percentage:  75 
+   },
+      {
+        course    : "HSC",
+        year      : "(2015-2017)",
+        Institute : "DVD Higher Secondary School",
+        percentage: 75
+  },
+      {
+        course    : "sslc",
+        year      : "(2014-2015)",
+        Institute : "SMRV Higher Secondary School",
+        percentage: 87 
+  }      
+   ],
+
+   Objective: "To contribute my positive attitude, adaptability, and strong communication skills to support team goals and deliver quality work", 
+   
+      
+   skills: [
+     "HTML",
+     "CSS",
+     "BOOTSTRAP"
+   ], 
+
+    certification:  [
+        {
+            course  : "fullstack web developer",
+            center  : "AGARAM ACADAMY",
+            place   : "nagercoil",
+            year    : 2024,
+            duration: "6_month"
+        }
+        ],
+
+   language:[
+        "tamil",
+        "english"
+    ],
+
+    work_experience: [ 
+        {
+            company_name: "DBMVIRCON",
+            experience  : "2_years",
+            year        : "2021_2023"
+    
+        }
+ ],
+     
+   project:[
+            { 
+                title       :"advanced mathematical reasoning",
+                company_name:"DBMVIRCON",
+                year        :2023,
+                discription :"mathematical is a reviewed open access journal meant to publish original and significant result and mathematical modelling and their application"
+            }
+        ],
+
+   personal_details:
+     {
+         father_name    :  "N.MURUGAN",
+         mother_name    :  "M.SELVI",
+         date_of_birth  :  "18-05-2000",
+         gender         :  "FEMALE",
+         nationality    :  "INDIAN",
+         marital_status :  "unmarried"  
+     },
+
+   declaration: "I hereby declare that the facts given above are genuine to the best of my knowledge and belief",
+  
+        
+        
+   date :"25-11-2024",
+   place:"nagercoil"  
+  
+  }
+console.log(resume.declaration);
